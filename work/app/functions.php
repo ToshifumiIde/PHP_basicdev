6e809ade4bbdc99c4576d3f48403752185bcd015
ToshifumiIde <toshifumiide198765@gmail.com>
2021-04-10 10:10:40 +0900

    createToken function and validateToken function

--- work/app/functions.php
+++ work/app/functions.php
@@ -6,3 +6,27 @@ function h($str)
 {
   return htmlspecialchars($str,ENT_QUOTES,"UTF-8");
 }
+
+function createToken()
+{
+  if(!isset($_SESSION["token"])){
+    $_SESSION["token"] = bin2hex(random_bytes(32));
+    //tokenが存在しなかった場合、randomで32bytesの文字列をtokenに格納する
+    //random_bytes()で生成される文字列はバイナリ文字列
+    //bin2hex()関数で16進数に変更
+  }
+}
+
+function validateToken()
+{
+  if(
+    empty(
+      $_SESSION["token"] || 
+      $_SESSION["token"] !== filter_input(INPUT_POST, "token")
+    )
+  ){
+    exit("Invalid post request");
+  }
+}
+
+session_start();
